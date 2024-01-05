7630581f8a60227e823738cd3d3e5e12915a38e2
Luciana Costilla <100047266+lucianacostilla@users.noreply.github.com>
2024-01-05 18:37:17 -0300

    Update bootstrap_variables.tf

--- modules/bootstrap/bootstrap_variables.tf
+++ modules/bootstrap/bootstrap_variables.tf
@@ -5,8 +5,6 @@
 variable "s3_nombre" {}
 variable "dynamo_db_nombre" {}
 variable "iam_user_name" {}
-variable "ado_iam_role_name" {
-  default = "rolAdmin"
-}
+variable "ado_iam_role_name" {}
 variable "aws_iam_policy_permits_name" {}
 variable "aws_iam_policy_assume_name" {}
